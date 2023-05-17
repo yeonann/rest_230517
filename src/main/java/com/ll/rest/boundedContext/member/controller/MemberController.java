b0d59d58dd4371c79c977699b9a16256e4f644d3
yeonann <yyeonann@gmail.com>
2023-05-18 01:56:39 +0900

    아이디, 비번으로 로그인 시 토큰을 받아야 함

--- src/main/java/com/ll/rest/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/rest/boundedContext/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.ll.rest.boundedContext.member.controller;
 
 import com.ll.rest.boundedContext.member.entity.Member;
 import com.ll.rest.boundedContext.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -28,7 +29,8 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-        public Member login(@Valid @RequestBody LoginRequest loginRequest) { // @RequestBody를 통해 json으로 받은 데이터를 해석할 수 있다.
+    public Member login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) { // @RequestBody를 통해 json으로 받은 데이터를 해석할 수 있다.
+        resp.addHeader("Authentication", "JWT 토큰");
             return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
         }
     }
