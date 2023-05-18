0f9a594a27bc53a5192d6c32311f2bff671c0c67
yeonann <yyeonann@gmail.com>
2023-05-18 13:43:10 +0900

    문서 꾸미기

---
+++ src/main/java/com/ll/rest/base/springDoc/SpringDocConfig.java
@@ -0,0 +1,10 @@
+package com.ll.rest.base.springDoc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "REST API", version = "v1"))
+public class SpringDocConfig {
+}
--- src/main/java/com/ll/rest/boundedContext/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/rest/boundedContext/member/controller/ApiV1MemberController.java
@@ -3,6 +3,8 @@ package com.ll.rest.boundedContext.member.controller;
 import com.ll.rest.base.rsData.RsData;
 import com.ll.rest.boundedContext.member.entity.Member;
 import com.ll.rest.boundedContext.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@Tag(name = "ApiV1MemberController", description = "로그인, 로그인 된 회원의 정보")
 public class ApiV1MemberController {
     private final MemberService memberService;
 
@@ -37,6 +40,7 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인, 엑세스 토큰 발급")
     public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
@@ -55,6 +59,7 @@ public class ApiV1MemberController {
 
     // consumes = ALL_VALUE => 나는 딱히 JSON 을 입력받기를 고집하지 않겠다.
     @GetMapping(value = "/me", consumes = ALL_VALUE)
+    @Operation(summary = "로그인된 사용자의 정보")
     public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
         Member member = memberService.findByUsername(user.getUsername()).get();
 
