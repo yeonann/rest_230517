2a9fbdee3b81c116382b2f6578aa326e8d197e47
yeonann <yyeonann@gmail.com>
2023-05-18 17:08:23 +0900

    글 쓰기

--- src/main/java/com/ll/rest/boundedContext/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/ll/rest/boundedContext/article/controller/ApiV1ArticlesController.java
@@ -3,15 +3,20 @@ package com.ll.rest.boundedContext.article.controller;
 import com.ll.rest.base.rsData.RsData;
 import com.ll.rest.boundedContext.article.entity.Article;
 import com.ll.rest.boundedContext.article.service.ArticleService;
+import com.ll.rest.boundedContext.member.entity.Member;
+import com.ll.rest.boundedContext.member.repository.MemberRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +28,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Tag(name = "ApiV1ArticlesController", description = "게시물 CRUD 컨트롤러")
 public class ApiV1ArticlesController {
     private final ArticleService articleService;
+    private final MemberRepository memberService;
 
     @AllArgsConstructor
     @Getter
@@ -60,5 +66,37 @@ public class ApiV1ArticlesController {
                 "%d번 게시물은 존재하지 않습니다.".formatted(id),
                 null
         ));
+    }
+
+    @Data
+    public static class WriteRequest {
+        @NotBlank
+        private String subject;
+        @NotBlank
+        private String content;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteResponse {
+        private final Article article;
+    }
+
+    @PostMapping(value = "")
+    @Operation(summary = "등록", security = @SecurityRequirement(name = "bearerAuth"))
+    public RsData<WriteResponse> write(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestBody WriteRequest writeRequest
+    ) {
+        Member member = memberService.findByUsername(user.getUsername()).orElseThrow();
+        RsData<Article> writeRs = articleService.write(member, writeRequest.getSubject(), writeRequest.getContent());
+
+        if (writeRs.isFail()) return (RsData) writeRs;
+
+        return RsData.of(
+                writeRs.getResultCode(),
+                writeRs.getMsg(),
+                new WriteResponse(writeRs.getData())
+        );
     }
 }
